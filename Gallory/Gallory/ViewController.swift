b5aac70e3e0635d7b732038e0c4d3a4e90631562
reza farahani <reza.khmf@gmail.com>
2020-01-24 16:38:39 +0330

    initial

---
+++ Gallory/Gallory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallory
+//
+//  Created by Reza Farahani on 24/1/20.
+//  Copyright © 2020 Reza Farahani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
